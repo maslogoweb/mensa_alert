091be9121d5fe334b6482563c7e22840719e8f08
kawagotoweb <kawagotoweb@gmail.com>
2019-02-15 13:51:06 +0900

    [add] getschedules

--- routes/samples.js
+++ routes/samples.js
@@ -1,5 +1,8 @@
+'use strict';
 var express = require('express');
 var router = express.Router();
+
+var client = require('cheerio-httpcli');
 
 /* サンプルAPI①
  * http://localhost:3000/samples にGETメソッドのリクエストを投げると、
@@ -19,6 +22,41 @@ router.get('/hello', function(req, res, next) {
 	var param = { result: 'Hello World !' };
 	res.header('Content-Type', 'application/json; charset=utf-8');
 	res.send(param);
+});
+
+/* サンプルAPI3
+ * http://localhost:3000/samples/mensa にGETメソッドのリクエストを投げると、
+ * JSON形式で文字列を返す。
+ */
+router.get('/getschedules', function(req, res, next) {
+	var result_array = new Array();
+	client.fetch('https://mensa.jp/exam/', function(err, $, res, body) {
+		// リンク一覧を表示
+		$('ul').each(function(idx) {
+			var prefecture = $(this)
+				.text()
+				.match(/[\u30e0-\u9fcf]*[都,道,府,県]/i);
+			var datetime = $(this)
+				.text()
+				.match(/日時 ： (.*)\n/i);
+			var state = $(this)
+				.children()
+				.find('img')
+				.attr('alt');
+			if (prefecture != null) {
+				result_array.push({
+					prefecture: prefecture[0],
+					datetime: datetime[1],
+					state: state
+				});
+			}
+		});
+	});
+	setTimeout(() => {
+		var param = { result: result_array };
+		res.header('Content-Type', 'application/json; charset=utf-8');
+		res.send(param);
+	}, 2000);
 });
 
 module.exports = router;
